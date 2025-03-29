0a6ada3847115a631fe22b76480da374d3c61dee
NoelTai <nnn122nnn70@gmail.com>
2025-03-29 19:28:22 +0800

    HW16 -done

--- HW/test_16/test_16.c
+++ HW/test_16/test_16.c
@@ -9,6 +9,13 @@ void str_shift(char *s, int shift){
     }
 }
 
+void str_empty(char *s){
+    int len = strlen(s);
+    for (int i = 0; i < len; i++){
+        s[i] = '\0';
+    }
+}
+
 void remove_zeros(char *s){
     int i = 0;
     while (s[i] == '0') i++;
@@ -47,6 +54,10 @@ void big_integer_add(char* n1, char* n2, char *sum){
             carry = 0;
         }
         sum[i] = (char)digit_sum + 48;
+        if (i == 0 && carry == 1){
+            str_shift(sum, 1);
+            sum[0] = '1';
+        }
     }
 }
 
@@ -63,7 +74,8 @@ void big_integer_sub(char *n1, char *n2, char *sum){
         }
     }
     else{
-        len = len2 - len1;
+        len = len2;
+        shift = len2 - len1;
         str_shift(n1, shift);
         neg_flag = 1;
     }
@@ -93,22 +105,105 @@ void big_integer_sub(char *n1, char *n2, char *sum){
         }
         sum[i] = (char)digit_sum + 48;
     }
-    printf("-");
+    if (neg_flag == 1){
+        printf("-");
+    }
 }
 
 void big_integer_mul(char *n1, char *n2, char *sum){
-    
+    int len1 = strlen(n1), len2 = strlen(n2);
+    int mul_sum = 0;
+    int data[201] = {0};
+    for (int i = len1 - 1; i > -1; i--){
+        for (int j = len2 - 1; j > -1; j--){
+            mul_sum = ((int)n1[i] - 48) * ((int)n2[j] - 48) + data[i+j+1];
+            data[i+j+1] = mul_sum % 10;
+            data[i+j] += mul_sum / 10;
+        }
+    }
+    sum[len1+len2] = '\0';
+    for (int i = 0; i < len1+len2; i++){
+        sum[i] = (char)data[i] + 48;
+    }
 }
 
 int main(){
-    char n1[50] = {}, n2[50] = {}, sum[50] = {};
+    char n1[102] = {}, n2[102] = {}, sum[201] = {};
+    int flag = 0;   // 0 => ++, 1 => +-, 2 => -+, 3 => --
     gets(n1);
     gets(n2);
+    if (n1[0] != '-' && n2[0] != '-'){
+        big_integer_add(n1, n2, sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_sub(n1, n2, sum);
+        remove_zeros(sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_mul(n1, n2, sum);
+        remove_zeros(sum);
+        printf("%s", sum);
+    }
+    else if (n1[0] != '-' && n2[0] == '-'){
+        n2[0] = '0';
+        remove_zeros(n2);
+        big_integer_sub(n1, n2, sum);
+        remove_zeros(sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_add(n1, n2, sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_mul(n1, n2, sum);
+        remove_zeros(sum);
+        printf("-%s\n", sum);
+    }
+    else if (n1[0] == '-' && n2[0] != '-'){
+        n1[0] = '0';
+        remove_zeros(n1);
+        big_integer_sub(n2, n1, sum);
+        remove_zeros(sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_add(n1, n2, sum);
+        printf("-%s\n", sum);
+        str_empty(sum);
+        big_integer_mul(n1, n2, sum);
+        remove_zeros(sum);
+        printf("-%s\n", sum);
+    }
+    else if (n1[0] == '-' && n2[0] == '-'){
+        n1[0] = '0';
+        n2[0] = '0';
+        remove_zeros(n1);
+        remove_zeros(n2);
+        big_integer_add(n1, n2, sum);
+        printf("-%s\n", sum);
+        str_empty(sum);
+        big_integer_sub(n2, n1, sum);
+        remove_zeros(sum);
+        printf("%s\n", sum);
+        str_empty(sum);
+        big_integer_mul(n1, n2, sum);
+        remove_zeros(sum);
+        printf("%s\n", sum);
+    }
 
-    big_integer_add(n1, n2, sum);
-    printf("%s\n", sum);
-    big_integer_sub(n1, n2, sum);
-    remove_zeros(sum);
-    printf("%s\n", sum);
+    // if (flag == 0){
+
+    // }
+
+    // big_integer_add(n1, n2, sum);
+    // printf("%s\n", sum);
+    // str_empty(sum);
+
+    // big_integer_sub(n1, n2, sum);
+    // remove_zeros(sum);
+    // printf("%s\n", sum);
+    // str_empty(sum);
+
+    // big_integer_mul(n1, n2, sum);
+    // remove_zeros(sum);
+    // printf("%s", sum);
     return 0;
 }
